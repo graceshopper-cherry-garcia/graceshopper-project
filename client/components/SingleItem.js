250413fc66a71b946449a3f01d1105c6f662302c
gbfrid <gabefridkis@gmail.com>
2021-08-10 16:30:01 -0500

    alert working for logged in user

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchItem, deleteItemThunk } from '../store/singleItem';
-import { addOrderItem } from '../store/orderItem';
-import order from '../store/order';
-import { fetchOrder } from '../store/order';
-import { fetchOrderItems } from '../store/cartOrderItems';
-
-
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchItem, deleteItemThunk } from "../store/singleItem";
+import { addOrderItem } from "../store/orderItem";
+import order from "../store/order";
+import { fetchOrder } from "../store/order";
+import { fetchOrderItems } from "../store/cartOrderItems";
 
 export class SingleItem extends React.Component {
   constructor(props) {
@@ -31,12 +29,20 @@ export class SingleItem extends React.Component {
       purchasePrice: this.props.item.price,
       itemId: this.props.item.id,
       user: this.props.user,
-    }
-    if(this.props.user.username) {
+    };
+    if (this.props.user.username) {
       event.preventDefault();
-      this.props.addToCart(cartItem);
       await this.props.getOrder(this.props.user.id);
-      await this.props.getOrderItems(this.props.order.id)
+      await this.props.getOrderItems(this.props.order.id);
+      let found;
+      found = this.props.orderItems.find((item) => {
+        return item.itemId === this.props.item.id;
+      });
+      if (found) {
+        alert("This item aleady exists in your cart!");
+      } else {
+        this.props.addToCart(cartItem);
+      }
     } else {
       const guestCartItem = {
         quantity: cartItem.quantity,
@@ -45,17 +51,20 @@ export class SingleItem extends React.Component {
         id: this.props.item.id,
         description: this.props.item.description,
         name: this.props.item.name,
-        price: this.props.item.price
-      }
+        price: this.props.item.price,
+      };
       event.preventDefault();
-      let cart = JSON.parse(window.localStorage.getItem('cart'));
+      let cart = JSON.parse(window.localStorage.getItem("cart"));
       let existingItems = cart.items;
       existingItems.push(guestCartItem);
-      window.localStorage.setItem('cart', JSON.stringify({'items': existingItems}))
-      }
-      this.setState({
-        addedToCart: true,
-      });
+      window.localStorage.setItem(
+        "cart",
+        JSON.stringify({ items: existingItems })
+      );
+    }
+    this.setState({
+      addedToCart: true,
+    });
   }
 
   handleChange(evt) {
@@ -76,11 +85,8 @@ export class SingleItem extends React.Component {
     return array;
   }
 
-
   render() {
-    console.log(this.props)
     const item = this.props.item;
-
     return (
       <div className="single-item-container">
         <img width="200px" src={item.imageUrl} />
@@ -118,7 +124,7 @@ export class SingleItem extends React.Component {
             </button>
           </div>
         )}
-              {this.state.addedToCart && (
+        {this.state.addedToCart && (
           <div
             className="alert alert-secondary alert-dismissible fade show"
             role="alert"
@@ -155,9 +161,7 @@ const mapDispatch = (dispatch, { history }) => {
     deleteItem: (item) => dispatch(deleteItemThunk(item, history)),
     addToCart: (orderItem) => dispatch(addOrderItem(orderItem)),
     getOrder: (userId) => dispatch(fetchOrder(userId)),
-    getOrderItems: (orderId) => dispatch(fetchOrderItems(orderId))
-
-
+    getOrderItems: (orderId) => dispatch(fetchOrderItems(orderId)),
   };
 };
 
