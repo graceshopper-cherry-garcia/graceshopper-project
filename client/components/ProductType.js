1a5df2620e693a676040b0ca36e92fad0304576f
Jose_Fernandez <jfernand84@gmail.com>
2021-08-09 22:28:13 -0400

    able to render image on top of image -intend to display dynamic background image next

--- client/components/ProductType.js
+++ client/components/ProductType.js
@@ -4,12 +4,14 @@ export class ProductType extends React.Component {
   constructor(props) {
     super(props);
   }
-  render(){
-    const item = this.props.item
-    return(
-      <div className="image-container">
-          <img width="20%" height= "20%" src={item.imageUrl} />
+  render() {
+    const item = this.props.item;
+    return (
+      <div className="outer-image-container">
+        <img src="https://image.shutterstock.com/image-vector/blank-tshirt-template-vector-260nw-161243906.jpg" />{' '}
+        <img className="inner-image" width="20%" height="20%" src={item.imageUrl} />
+        {/* <div className="inner-image"> */}
       </div>
-    )
+    );
   }
 }
